8b48ac430a3c47063167985794cf9cf7ff9e5bde
cedrickjames <cedrickorozo@gmail.com>
2023-03-13 21:02:53 +0800

    offline tailwind is not working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+]
+}
