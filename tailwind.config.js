0c0b7200a7a05554900c802e6f26f1b5ddff91cb
cedrickjames <cedrickorozo@gmail.com>
2023-03-22 15:13:34 +0800

    changin the design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        // Simple 8 row grid
+        '12': 'repeat(12, minmax(0, 1fr))',
+       
+        // Complex site-specific row configuration
+        // 'layout': '200px minmax(900px, 1fr) 100px',
+      },
+      gridRow: {
+        'span-11': 'span 11 / span 11',
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
